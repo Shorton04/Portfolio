9f1980b62cf7db8844c63bbb9621fb635caa9706
Shorton04 <120240879+Shorton04@users.noreply.github.com>
2025-03-28 17:30:56 +0800

    Initial commit

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# build_files.sh
-echo "Building the project..."
-python3 -m pip install -r requirements.txt
-python3 manage.py collectstatic --noinput
+# vercel.build.sh
+export PATH=$PATH:/vercel/.local/bin
+pip install -r requirements.txt
+python manage.py collectstatic --noinput
