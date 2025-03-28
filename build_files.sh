f213747c3949506673bd72cf8a62c511e283432c
Shorton04 <120240879+Shorton04@users.noreply.github.com>
2025-03-28 17:28:12 +0800

    Initial commit

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
 # build_files.sh
-pip install -r requirements.txt
-python manage.py collectstatic --noinput
+echo "Building the project..."
+python3 -m pip install -r requirements.txt
+python3 manage.py collectstatic --noinput
